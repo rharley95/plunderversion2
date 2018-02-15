84a633b53bd0fe0431980402ea7966781c0df2b7
denzy134 <35541187+denzy134@users.noreply.github.com>
2018-02-15 14:06:28 -0700

    COmplete test cases

--- test/citbyui/cit260piratesgame/control/CrewControlTest.java
+++ test/citbyui/cit260piratesgame/control/CrewControlTest.java
@@ -31,11 +31,11 @@ public class CrewControlTest {
            //input variables     
 
         
-        double length = 9.0;
+        double length = 11.0;
         double height = 5.0;
         
         CrewControl instance = new CrewControl();
-        double expResult = 10;
+        double expResult = 12;
         
         // call method
         double result = instance.catchCrewMemeber(length, height);
@@ -43,7 +43,119 @@ public class CrewControlTest {
         //test results
         
         assertEquals(expResult, result, 0.0);
+       
+         System.out.println("\tTest case 2");
         
+           //input variables     
+
+        
+         length = -2;
+         height = 10;
+        
+       
+         expResult = -1;
+        
+        // call method
+         result = instance.catchCrewMemeber(length, height);
+        
+        //test results
+        
+        assertEquals(expResult, result, 0.0);
+                
+
+         //-testcase--
+        
+        System.out.println("\tTest case 3");
+        
+           //input variables     
+
+        
+         length = 11.0;
+         height = -4;
+        
+      
+         expResult = -2;
+        
+        // call method
+        result = instance.catchCrewMemeber(length, height);
+        
+        //test results
+        
+        assertEquals(expResult, result, 0.0);
+        
+        
+        System.out.println("\tTest case 4");
+        
+           //input variables     
+
+        
+         length = 17.0;
+         height = 9;
+        
+      
+         expResult = -1;
+        
+        // call method
+        result = instance.catchCrewMemeber(length, height);
+        
+        //test results
+        
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("\tTest case 5");
+        
+           //input variables     
+
+        
+         length = 1;
+         height = 10;
+        
+      
+         expResult = 10;
+        
+        // call method
+        result = instance.catchCrewMemeber(length, height);
+        
+        //test results
+        
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("\tTest case 6");
+        
+           //input variables     
+
+        
+         length = 11.0;
+         height = 1;
+        
+      
+         expResult = 11;
+        
+        // call method
+        result = instance.catchCrewMemeber(length, height);
+        
+        //test results
+        
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("\tTest case 7");
+        
+           //input variables     
+
+        
+         length = 11.0;
+         height = 10;
+        
+      
+         expResult = 15;
+        
+        // call method
+        result = instance.catchCrewMemeber(length, height);
+        
+        //test results
+        
+        assertEquals(expResult, result, 0.0);
+   
     }
         
         // TODO review the generated test code and remove the default call to fail.
