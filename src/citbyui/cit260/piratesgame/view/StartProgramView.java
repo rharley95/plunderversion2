2cd0e8d0aa736bc72dfe43d094127bd81dc9abc6
denzy134 <35541187+denzy134@users.noreply.github.com>
2018-02-26 13:01:39 -0700

    start program

--- src/citbyui/cit260/piratesgame/view/StartProgramView.java
+++ src/citbyui/cit260/piratesgame/view/StartProgramView.java
@@ -10,38 +10,63 @@ package citbyui.cit260.piratesgame.view;
  * @author rominapainter
  */
 public class StartProgramView {
-        
-        public StartProgramView() {
-            
-}
-        
-        public void DisplayStartProgramView() {
-//            endView = false
-//            DO
-//                    inputs = getInputs();
-//            if (no inputs OR first input is "Q")
-//                RETURN
-//                endView = doAction(inputs)
-//            WHILE endView != true
-            String[] inputs = this.getInputs();
-        }
 
-    private String[] getInputs() {
-        System.out.println("**** getInputs() called****"); 
-        
-        String[] inputs = new String[1];
-        inputs[0] = "testInputs";
-        
-        return inputs;
+    public StartProgramView() {
+
     }
     
-    private boolean doAction(String[] inputs) {
-        System.out.println("**** doAction() called****"); 
-        System.out.println("\tinputs = " + inputs[0]); 
+    public void displayStartProgramView() {
+           boolean endView = false;
+            do {
+                   String[] inputs = getInputs();
+           if (inputs[].length < 1 || inputs[0] == "Q")
+           {
+                return endView = doAction(inputs);
+               }
+            while (endView != true);
+    
+    getInputs(): String[] {
 
-        
+ inputs = new String array one element long
+ Display the welcome banner
+
+valid = false
+WHILE valid == false (while input value is not valid)
+
+Display the prompt message, "Enter the player’s name"
+Get the value entered from the keyboard
+Trim off leading and trailing blanks from the value
+IF length of the value < 2 then
+Display "You must enter a value.”
+Continue (move to the top of the loop and repeat)
+ENDIF
+Assign the value to the first position in the inputs array
+valid = true
+ENDWHILE
+
+RETURN inputs
+}
+        String[] inputs = this.getInputs();
+    }
+
+    private String[] getInputs() {
+        System.out.println("**** getInputs() called****");
+
+        String[] inputs = new String[1];
+        inputs[0] = "testInputs";
+
+        return inputs;
+    }
+
+    private boolean doAction(String[] inputs) {
+        System.out.println("**** doAction() called****");
+        System.out.println("\tinputs = " + inputs[0]);
+
         return true;
     }
-        
-            
+
+    public void display() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
