2473487b1ebb71fb6e5532ce66663c89c7229a22
denzy134 <35541187+denzy134@users.noreply.github.com>
2018-02-27 13:06:53 -0700

    feb 27

--- src/citbyui/cit260/piratesgame/view/StartProgramView.java
+++ src/citbyui/cit260/piratesgame/view/StartProgramView.java
@@ -4,68 +4,63 @@
  * and open the template in the editor.
  */
 package citbyui.cit260.piratesgame.view;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-
 
 /**
  *
  * @author rominapainter
  */
-public class StartProgramView implements Serializable{
+public class StartProgramView implements Serializable {
 
     public StartProgramView() {
 
     }
-    
+
     private ArrayList<StartProgramView> inputs = new ArrayList<StartProgramView>();
-    
+
     public void displayStartProgramView() {
-        
-           boolean endView = false;
-           
-            do {
-                   String[] inputs = this.getInputs();
-                   
-            if (inputs[0].length() < 1 || inputs[0].equals('Q')){
+
+        boolean endView = false;
+
+        do {
+            String[] inputs = this.getInputs();
+
+            if (inputs[0].length() < 1 || inputs[0].equals('Q')) {
                 return;
-               }
+            }
             endView = doAction(inputs);
             doAction(inputs);
-            
-            }
-            while (endView != true);
+
+        } while (endView != true);
     }
 
     private String[] getInputs() {
-        
 
         String[] inputs = new String[1];
-        System.out.println("banner message here");
+        System.out.println("Welcome to the game of the pirates");
 
         boolean valid = false;
+        Scanner name = new Scanner(System.in);
+        while (valid == false) {
 
-        while (valid == false){
+            System.out.println("Enter the player’s name");
+            String playername = name.nextLine();
+            String playertrim = playername.trim();
 
-             System.out.println("Enter the player’s name");
-             Scanner name = new Scanner(System.in);
-             String playername = name.nextLine();
-             String playertrim = playername.trim();
-             
-        if(playername.length() < 2 ){
-            System.out.println("You must enter a better name.");
-            continue;
-        }
-        
-        inputs[0] = playertrim;
-        valid = true;
+            if (playertrim.length() < 2) {
+                System.out.println("You must enter a better name.");
+                continue;
+            }
+
+            inputs[0] = playertrim;
+            valid = true;
         }
 
         return inputs;
-        }
-
+    }
 
     private boolean doAction(String[] inputs) {
         System.out.println("**** doAction() called****");
@@ -75,5 +70,3 @@ public class StartProgramView implements Serializable{
     }
 
 }
-
- 
